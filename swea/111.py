e953b529c9852d9cdced9aae353249a1d2a9aa0f
kkammaneo <rkdalsdn0914@naver.com>
2025-03-01 23:24:47 +0900

    clear

--- swea/111.py
+++ swea/111.py
@@ -2,11 +2,9 @@ numbers = [1,1,1,1,1]
 target = 3
 
 check1 = [0] * len(numbers)
-
-
-def dfs(depth, check):
+answer = 0
+def dfs(depth, check, answer):
     result = 0
-    answer = 0
     if depth == len(numbers):
         for i in range(len(numbers)):
             if check[i] == 0:
@@ -18,11 +16,34 @@ def dfs(depth, check):
         return
 
     check[depth] = 0
-    dfs(depth + 1, check)
+    dfs(depth + 1, check, answer)
 
     check[depth] = 1
-    dfs(depth + 1, check)
+    dfs(depth + 1, check, answer)
 
-
-print(dfs(0, check1))
+real_result = 0
+dfs(0,check1,0)
 print(answer)
+#
+# def dfs(depth, check):
+#     global answer
+#     result = 0
+#     if depth == len(numbers):
+#         for i in range(len(numbers)):
+#             if check[i] == 0:
+#                 result -= numbers[i]
+#             else:
+#                 result += numbers[i]
+#         if result == target:
+#             answer += 1
+#         return answer
+#
+#     check[depth] = 0
+#     dfs(depth + 1, check)
+#
+#     check[depth] = 1
+#     dfs(depth + 1, check)
+#     return answer
+# real_result = 0
+# dfs(0,check1)
+# print(answer)
